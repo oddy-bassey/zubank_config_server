36410ce94e99460150f0c56e988a55a7f290815a
oddy-bassey <oddybassey@gmail.com>
2022-06-13 13:01:46 +0100

    initial project setup

---
+++ src/main/java/com/revoltcode/zubank_config_server/ZubankConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.revoltcode.zubank_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZubankConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZubankConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revoltcode/zubank_config_server/ZubankConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revoltcode.zubank_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZubankConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
